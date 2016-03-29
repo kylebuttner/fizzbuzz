53a9a897d683cb2ea19aff785394d384b8392120
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-29 11:47:18 +0100

    Created inital tests

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,9 @@
+describe("fizzBuzz", function() {
+  it("returns 'fizz' when passed 3", function() {
+    expect(fizzBuzz(3)).toEqual("fizz");
+  });
+
+  it("returns 1 when passed 1", function() {
+    expect(fizzBuzz(1)).toEqual(1);
+  });
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,7 @@
+function fizzBuzz(number) {
+  if (number === 1) {
+    return 1;
+  } else {
+    return "fizz";
+  }; 
+}
