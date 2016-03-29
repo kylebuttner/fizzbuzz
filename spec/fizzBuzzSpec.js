f3da0d2f67228324e4c0d9f5c94e7c99f41d0e18
Kyle Buttner <kylebuttner@gmail.com>
2016-03-29 11:57:17 +0100

    finish fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -6,4 +6,19 @@ describe("fizzBuzz", function() {
   it("returns 1 when passed 1", function() {
     expect(fizzBuzz(1)).toEqual(1);
   });
+  it("returns buzz when passed 5", function() {
+    expect(fizzBuzz(5)).toEqual("buzz");
+  });
+  it("returns 4 when passed 4", function() {
+    expect(fizzBuzz(4)).toEqual(4);
+  });
+  it("returns fizz when number is divisible by 3", function() {
+    expect(fizzBuzz(12)).toEqual("fizz");
+  });
+  it("returns buzz when number is divisible by 5", function() {
+    expect(fizzBuzz(10)).toEqual("buzz");
+  });
+  it("returns fizzbuzz when number is divisible by 3 and 5", function() {
+    expect(fizzBuzz(15)).toEqual("fizzbuzz");
+  });
 });
